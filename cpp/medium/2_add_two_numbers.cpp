63e8a898860f4cfbf4a02294b4bc531272ba4c98
ChloeCrozier <chloecrozier@gmail.com>
2024-05-07 09:59:32 -0400

    #2

--- cpp/medium/2_add_two_numbers.cpp
+++ cpp/medium/2_add_two_numbers.cpp
@@ -1,4 +1,4 @@
-// https://leetcode.com/problems/add-two-numbers/
+k// https://leetcode.com/problems/add-two-numbers/
 class Solution {
 public:
     void fixOverflow(ListNode *ls){
@@ -53,3 +53,37 @@ public:
         return head;
     }
 };
+
+
+// // Simple secondary way (not in-place so it has worse space complexity).
+// class Solution {
+// public:
+//     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+//         ListNode *head = new ListNode(0);
+//         ListNode *iter = head;
+//         int overflow = 0;
+//         int sum = 0;
+//         while(l1 != NULL || l2 != NULL || overflow != 0){
+//             if(l1 != NULL){
+//                 sum += l1->val;
+//             }
+//             if(l2 != NULL){
+//                 sum += l2->val;
+//             }
+//             sum += overflow;
+//             overflow = sum / 10;
+//             iter->next = new ListNode(sum % 10);
+//             overflow = sum / 10;
+
+//             if(l1 != NULL){
+//                 l1 = l1->next;
+//             }
+//             if(l2 != NULL){
+//                 l2 = l2->next;
+//             }
+//             iter = iter->next;
+//             sum = 0;
+//         }
+//         return head->next;
+//     }
+// };
