8aa112df7828403bc173f55ec75d01bdb2cd62f1
ChloeCrozier <chloecrozier@gmail.com>
2024-05-08 12:26:07 -0400

    #1290

---
+++ cpp/easy/1290_convert_binary_number_in_a_linked_list_to_integer.cpp
@@ -0,0 +1,32 @@
+// https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-integer/description/
+class Solution {
+public:
+    int getDecimalValue(ListNode* head) {
+        int num = 0;
+        while(head != NULL){
+            num = (num << 1) + head->val;
+            head = head->next;
+        }
+        return num;
+    }
+
+    // int getIndex(ListNode* head){
+    //     if(head->next == NULL){
+    //         return 0;
+    //     }
+    //     return 1 + getIndex(head->next);
+    // }
+
+    // int getDecimalValue(ListNode* head) {
+    //     int power = getIndex(head);
+    //     int num = 0;
+    //     while(head != NULL){
+    //         if(head->val == 1){
+    //             num += pow(2, power);
+    //         }
+    //         head = head->next;
+    //         power--;
+    //     }
+    //     return num;
+    // }
+};
