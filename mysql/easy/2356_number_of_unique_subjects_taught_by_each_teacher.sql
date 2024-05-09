42d93668fd9d41bf4185393f9a926814079c46a6
Chloe Crozier <40836363+ChloeCrozier@users.noreply.github.com>
2024-05-09 14:19:58 -0400

    #2356

--- mysql/easy/2356_number_of_unique_subjects_taught_by_each_teacher.sql
+++ mysql/easy/2356_number_of_unique_subjects_taught_by_each_teacher.sql
@@ -1,6 +1,6 @@
 # https://leetcode.com/problems/number-of-unique-subjects-taught-by-each-teacher/description/
 SELECT
-	teacher_id, COUNT(DISTINCT subject_id) as cnt
+	teacher_id, COUNT(DISTINCT subject_id) as cnts
 FROM
 	Teacher
 GROUP BY
