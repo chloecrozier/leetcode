25d7c7e7b9865c074aa7e383f7f65e5b85231423
ChloeCrozier <chloecrozier@gmail.com>
2024-05-10 18:58:43 -0400

    #160

---
+++ mysql/medium/180_consecutive_numbers.sql
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/consecutive-numbers/description/
+SELECT
+    DISTINCT L1.num as ConsecutiveNums
+FROM
+    Logs AS L1
+JOIN
+    Logs AS L2
+ON
+    L1.id + 1 = L2.id
+JOIN
+    Logs AS L3
+ON
+    L1.id + 2 = L3.id
+WHERE
+    L1.num = L2.num AND L1.num = L3.num;
