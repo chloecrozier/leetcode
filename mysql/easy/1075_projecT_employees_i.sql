d357362398e183ae8ca92ba4a1f923ac98514af6
ChloeCrozier <chloecrozier@gmail.com>
2024-05-10 15:23:49 -0400

    #1075

---
+++ mysql/easy/1075_projecT_employees_i.sql
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/project-employees-i/description/
+SELECT
+    P.project_id, ROUND(AVG(E.experience_years), 2) AS average_years
+FROM
+    Employee AS E
+JOIN
+    Project AS P
+ON
+    E.employee_id = P.employee_id
+GROUP BY
+    P.project_id;
