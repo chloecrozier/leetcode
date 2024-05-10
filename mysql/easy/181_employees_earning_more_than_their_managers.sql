bc053e4fb9a4713d8265796eb5e5b2da9d03b4ff
ChloeCrozier <chloecrozier@gmail.com>
2024-05-09 21:14:19 -0400

    #181

---
+++ mysql/easy/181_employees_earning_more_than_their_managers.sql
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/employees-earning-more-than-their-managers/description/
+SELECT
+    E.name AS Employee
+FROM
+    Employee E
+JOIN
+    Employee M
+ON
+    E.managerID = M.id
+WHERE
+    E.salary > M.salary
