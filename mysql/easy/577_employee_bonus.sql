20bafee94802955988063949bf57dabf562a760b
ChloeCrozier <chloecrozier@gmail.com>
2024-05-10 11:05:04 -0400

    #577

---
+++ mysql/easy/577_employee_bonus.sql
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/employee-bonus/description/
+SELECT
+    E.name, B.bonus
+FROM
+    Employee AS E
+LEFT JOIN
+    Bonus AS B
+ON
+    E.empId = B.empId
+WHERE
+    B.bonus < 1000 OR B.bonus IS NULL;
