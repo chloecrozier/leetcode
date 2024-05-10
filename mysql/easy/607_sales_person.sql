8fc307a493c05c533a72af72e56ecf83cf4ee11d
ChloeCrozier <chloecrozier@gmail.com>
2024-05-10 11:50:18 -0400

    #607

---
+++ mysql/easy/607_sales_person.sql
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/sales-person/description/
+SELECT
+    name
+FROM
+    SalesPerson
+WHERE
+    sales_id NOT IN (
+        SELECT
+            sales_id
+        FROM
+            Orders
+        JOIN
+            Company
+        ON
+            Orders.com_id = Company.com_id
+        WHERE
+            Company.name = 'RED'
+    );
