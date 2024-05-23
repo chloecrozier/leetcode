43bca36ac0e249638ec4557bf29335da9a8fb052
ChloeCrozier <chloecrozier@gmail.com>
2024-05-23 17:34:32 -0400

    #2824

--- cpp/easy/2824_count_pairs_whose_sum_is_less_than_target.cpp
+++ cpp/easy/2824_count_pairs_whose_sum_is_less_than_target.cpp
@@ -13,3 +13,24 @@ public:
         return count;
     }
 };
+
+
+// https://leetcode.com/problems/count-pairs-whose-sum-is-less-than-target/submissions/1265362287/
+class Solution {
+public:
+    int countPairs(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        int count = 0;
+        int left = 0;
+        int right = nums.size() - 1;
+        while(left < right){
+            if(nums[left] + nums[right] < target){
+                count += right - left;
+                left++;
+            } else{
+                right--;
+            }
+        }
+        return count;
+    }
+};
