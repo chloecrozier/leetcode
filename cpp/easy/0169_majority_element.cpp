5d19c29420fb900b9753c1eaf17d588fcb0fe310
ChloeCrozier <chloecrozier@gmail.com>
2024-05-26 23:38:10 -0400

    #169

--- cpp/easy/0169_majority_element.cpp
+++ cpp/easy/0169_majority_element.cpp
@@ -2,7 +2,20 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        return nums[nums.size() / 2];
+        int candidate = 0;
+        int count = 0;
+        for(auto num : nums){
+            if(count == 0){
+                candidate = num;
+                count++;
+            } else{
+                if(candidate == num){
+                    count++;
+                } else{
+                    count--;
+                }
+            }
+        }
+        return candidate;
     }
 };
