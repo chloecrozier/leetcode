281d9a681f2b6178038e8957e4f0fb41b0492f43
ChloeCrozier <chloecrozier@gmail.com>
2024-07-29 23:58:06 -0400

    #415

---
+++ cpp/easy/415_add_strings.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string addStrings(string num1, string num2) {
+        int sum;
+        if(len(num1) > len(num2)){
+            for(int i = 0; i < num1; i++){
+                sum += num1[len(num1) - i] * tens;
+                tens *= 10;
+            }
+        } else{
+
+        }
+
+    }
+};
