5c3b8a47c1333422f4d281093a69a4065242fbec
ChloeCrozier <chloecrozier@gmail.com>
2025-02-08 22:11:52 -0500

    #2364

---
+++ python/medium/2364_count_number_of_bad_pairs.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/count-number-of-bad-pairs/description/?envType=daily-question&envId=2025-02-09
+class Solution:
+    def countBadPairs(self, nums: List[int]) -> int:
+        goodCount = 0
+        total = ((len(nums) - 1) * len(nums)) // 2
+        m = {}
+        for i in range(len(nums)):
+            if nums[i] - i in m:
+                m[nums[i] - i] += 1
+            else:
+                m[nums[i] - i] = 1
+
+        for key, value in m.items():
+            if value >= 2:
+                goodCount += (value * (value - 1)) // 2
+
+        return total - goodCount
