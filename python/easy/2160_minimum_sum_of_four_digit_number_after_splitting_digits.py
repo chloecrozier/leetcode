5777547ce42be35d8a4e8489bab94f4b9e172043
ChloeCrozier <chloecrozier@gmail.com>
2025-02-17 12:41:38 -0500

    #2160

--- python/easy/2160_minimum_sum_of_four_digit_number_after_splitting_digits.py
+++ python/easy/2160_minimum_sum_of_four_digit_number_after_splitting_digits.py
@@ -1,7 +1,9 @@
+k# https://leetcode.com/problems/minimum-sum-of-four-digit-number-after-splitting-digits/description/
 class Solution:
     def getSum(self, x, y, z, w):
         return ((10 * x) + y) + ((10 * z) + w)
 
     def minimumSum(self, num: int) -> int:
         num = [int(x) for x in str(num)]
+        num = sorted(num)
         return self.getSum(num[0], num[2], num[1], num[3])
