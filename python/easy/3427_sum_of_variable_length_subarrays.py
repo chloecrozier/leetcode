be9fbf6ca4133318d44ce2860318fc81f8ed0e74
ChloeCrozier <chloecrozier@gmail.com>
2025-03-30 18:16:43 -0400

    #3427

--- python/easy/3427_sum_of_variable_length_subarrays.py
+++ python/easy/3427_sum_of_variable_length_subarrays.py
@@ -1,8 +1,16 @@
 # https://leetcode.com/problems/sum-of-variable-length-subarrays/description/
 class Solution:
     def subarraySum(self, nums: List[int]) -> int:
-        res = 0
+        prefixSum = [nums[0]]
+        res = 0 
+        for i in range(1, len(nums)):
+            prefixSum.append(nums[i] + prefixSum[i - 1])
+
         for i in range(len(nums)):
-            for num in nums[max(0, i - nums[i]):i + 1]:
-                res += num
+            diff = i - nums[i]
+            if diff > 0:
+                res += prefixSum[i] - prefixSum[diff - 1]
+            else:
+                res += prefixSum[i]
+
         return res
