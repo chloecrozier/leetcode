9ce2c2219da490b865beb28bf36176846f7e89cf
ChloeCrozier <chloecrozier@gmail.com>
2025-05-12 10:17:57 -0400

    #1920

--- python/easy/1920_build_array_from_permutation.py
+++ python/easy/1920_build_array_from_permutation.py
@@ -2,3 +2,12 @@
 class Solution(object):
     def buildArray(self, nums):
         return [nums[nums[i]] for i in range(len(nums))]
+
+
+# beats 100%
+class Solution:
+    def buildArray(self, nums: List[int]) -> List[int]:
+        res = []
+        for i in range(len(nums)):
+            res.append(nums[nums[i]])
+        return res
