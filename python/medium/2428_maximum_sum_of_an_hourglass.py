1fe992e381b5f0e892060e7ca679e1d3bbe118a0
ChloeCrozier <chloecrozier@gmail.com>
2025-05-28 01:27:50 -0400

    #2428

--- python/medium/2428_maximum_sum_of_an_hourglass.py
+++ python/medium/2428_maximum_sum_of_an_hourglass.py
@@ -1,1 +1,9 @@
-https://leetcode.com/problems/maximum-sum-of-an-hourglass/submissions/1646670706/?envType=problem-list-v2&envId=prefix-sum
+# https://leetcode.com/problems/maximum-sum-of-an-hourglass/description/?envType=problem-list-v2&envId=prefix-sum
+class Solution:
+    def maxSum(self, m: List[List[int]]) -> int:
+        maxVal = 0
+        for i in range(len(m) - 2):
+            for j in range(len(m[i]) - 2):
+                currSum = m[i][j] + m[i][j + 1] + m[i][j + 2] + m[i + 1][j + 1] + m[i + 2][j] + m[i + 2][j + 1] + m[i + 2][j + 2]
+                maxVal = max(maxVal, currSum)
+        return maxVal
