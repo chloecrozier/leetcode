32376cf64501e0dbc0cd0bed7b6a396d1255447b
ChloeCrozier <chloecrozier@gmail.com>
2025-07-06 23:56:47 -0700

    #832

--- python/easy/0832_flipping_an_image.py
+++ python/easy/0832_flipping_an_image.py
@@ -4,10 +4,9 @@ class Solution:
         l = len(image[0])
         for i in range(len(image)):
             for j in range((l // 2)):
-                if l - j - 1 != j:
-                    image[i][j], image[i][l - j - 1] = image[i][l - j - 1], image[i][j]
-                    image[i][j] ^= 1
-                    image[i][l - j - 1] ^= 1
+                image[i][j], image[i][l - j - 1] = image[i][l - j - 1], image[i][j]
+                image[i][j] ^= 1
+                image[i][l - j - 1] ^= 1
             if l % 2:
                 image[i][l // 2] ^= 1
         return image
