455615325fb240e017574699f9bb500b6c6d804d
ChloeCrozier <chloecrozier@gmail.com>
2026-09-18 00:21:16 -0700

    #513

--- python/medium/0513_find_bottom_left_tree_value.py
+++ python/medium/0513_find_bottom_left_tree_value.py
@@ -1,22 +1,17 @@
 # https://leetcode.com/problems/find-bottom-left-tree-value/description/
 class Solution:
     def findBottomLeftValue(self, root: TreeNode | None) -> int:
-        def getDepth(root: TreeNode):
-            if root == None:
-                return 0
-            else:
-                depth = max(getDepth(root.left), getDepth(root.right)) + 1
-                return depth
-
-        maxDepth = getDepth(root)
-        lastRow = []
-
-        def search(root: TreeNode, currDepth: int):
-            if root != None:
-                if currDepth == maxDepth:
-                    lastRow.append(root.val)
-                search(root.left, currDepth + 1)
-                search(root.right, currDepth + 1)
+        maxDepth = -1
+        left = root.val
         
-        search(root, 1)
-        return lastRow[0]
+        def getDepth(root: TreeNode, currDepth: int):
+            if root != None:
+                nonlocal left, maxDepth
+                if maxDepth < currDepth:
+                    maxDepth = currDepth
+                    left = root.val
+                getDepth(root.left, currDepth + 1)
+                getDepth(root.right, currDepth + 1)
+
+        getDepth(root, 1)
+        return left
