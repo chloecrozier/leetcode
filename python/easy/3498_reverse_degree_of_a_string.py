7d561de7b69db89f8b25e0c43fd875058e5c1c52
ChloeCrozier <chloecrozier@gmail.com>
2026-09-19 22:12:46 -0700

    #3498

--- python/easy/3498_reverse_degree_of_a_string.py
+++ python/easy/3498_reverse_degree_of_a_string.py
@@ -5,3 +5,15 @@ class Solution:
         for i in range(len(s)):
             res += (i + 1) * (26 - (ord(s[i]) % 97))
         return res
+
+
+
+# Redone a year because I forgot I already did it until re-submitting to github
+# Redone because it was the probelm fo the day too. I imporved by not using 97 as a magic number
+# https://leetcode.com/problems/reverse-degree-of-a-string/description/?envType=daily-question&envId=2026-09-20
+class Solution:
+    def reverseDegree(self, s: str) -> int:
+        degree = 0
+        for i in range(len(s)):
+            degree += (26 - (ord(s[i]) - ord('a'))) * (i + 1)
+        return degree
